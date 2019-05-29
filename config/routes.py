eaaf63fd1ec51b476afb7985a33e6b6c7db8805d
Emma Price <ekprice01@gmail.com>
2019-05-29 10:56:44 +0100

    Can get pools through to browser on app js

--- config/routes.py
+++ config/routes.py
@@ -2,5 +2,5 @@ from app import app
 from controllers import pools, auth
 
 # loading it in to memory (RAM)
-app.register_blueprint(pools.router)
-app.register_blueprint(auth.router)
+app.register_blueprint(pools.router, url_prefix='/api')
+app.register_blueprint(auth.router, url_prefix='/api')
--- src/app.js
+++ src/app.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import axios from 'axios'
 
 class App extends React.Component {
+
+  componentDidMount() {
+    axios.get('/api/pools')
+      .then(res => this.setState({ pools: res.data }))
+  }
+
   render() {
+    if(!this.state) return <p>Loading...</p>
     return (
-      <h1>Hello World!</h1>
+      <div>
+        {this.state.pools.map(pool => <div key={pool.id}>
+          <h2>{pool.name}</h2>
+          <p>{pool.address}</p>
+          <p>{pool.description}</p>
+        </div>)}
+      </div>
     )
   }
 }
